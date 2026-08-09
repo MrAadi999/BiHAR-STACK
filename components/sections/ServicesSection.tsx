1f57fc8682da3a96f68460f9d57dee0a96063076
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2026-08-09 18:12:47 +0530

    style: shift sitting person character a tiny bit further right (-right-2 sm:-right-3)

--- components/sections/ServicesSection.tsx
+++ components/sections/ServicesSection.tsx
@@ -135,12 +135,12 @@ export default function ServicesSection() {
                   What We Offer
                 </h2>
 
-                {/* Clean Sitting Person Character (Shifted Slightly Right) */}
+                {/* Clean Sitting Person Character (Shifted A Tiny Bit More Right) */}
                 <motion.div 
                   initial={{ y: -8, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
                   transition={{ duration: 0.6, delay: 0.3 }}
-                  className="absolute -top-[44px] sm:-top-[64px] right-0 sm:right-1 w-11 sm:w-15 h-17 sm:h-24 pointer-events-none z-30"
+                  className="absolute -top-[44px] sm:-top-[64px] -right-2 sm:-right-3 w-11 sm:w-15 h-17 sm:h-24 pointer-events-none z-30"
                 >
                   <img 
                     src="/images/person_only_no_qmark.png" 
